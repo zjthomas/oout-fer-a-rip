dfd71844f381de93cb3b6adc3180ebd7a8475d31
zjthomas <zachthomas809@gmail.com>
2019-08-10 17:15:12 -0700

    end of day

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,7 +9,10 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <h1>HI</h1>
+        <div className="header"></div>
+        <div className="content">
+          <
+        </div>
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 const port = 3000;
 
-app.use('/',express.static(path.join(__dirname,'../dist')));
+app.use('/', express.static(path.join(__dirname, '../dist')));
 
 app.get('/', (req, res) => {
   res.end();
